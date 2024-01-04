a65fbb887fb05cf17f7f8ed6bc8e44369a2970e8
ludwigstrand <ludwig.strand@live.com>
2024-01-04 13:16:42 +0100

    databas

--- client/script/script.js
+++ client/script/script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 let setActiveElement = document.getElementById("addMovieForm"),
     btn = document.getElementById("addMovieButton");
 
@@ -5,3 +6,7 @@ let setActiveElement = document.getElementById("addMovieForm"),
 btn.addEventListener('click', () => {
     setActiveElement.classList.toggle("form--active");
 })
+=======
+
+const url = 'http://localhost:3000/movie/';
+>>>>>>> Stashed changes
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 /* Server objekt  */
 
 const server = express();
+const database = require("./netflix.db");
 
 /* Server inställningar */
 
@@ -23,4 +24,30 @@ server.listen(3000, () => {
 });
 sqlite3 = require("sqlite3").verbose();
 
+server.get("/movies", (req, res) => {
+  database.all("SELECT * FROM movies", (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: err });
+    } else {
+      res.status(200).json(rows);
+    }
+  });
+});
 
+server.put("/movies/:id", (req, res) => {
+  const id = req.params.id;
+  const movie = req.body;
+  database.run(
+    `UPDATE movies SET title=?, director=?, year=?, rating=? WHERE id=?`,
+    [movie.title, movie.director, movie.year, movie.rating, id],
+    (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ error: err });
+      } else {
+        res.status(200).json({ message: "Movie updated" });
+      }
+    }
+  );
+});
