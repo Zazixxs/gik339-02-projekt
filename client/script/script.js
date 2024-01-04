91740b85d38d6cf534891e00f2e2658f08bd8edb
Oliver Näs <nolivern@gmail.com>
2024-01-04 14:17:07 +0100

    Modal

--- client/script/script.js
+++ client/script/script.js
@@ -1,3 +1,7 @@
+
+
+// --------------- Site functionality --------------- //
+
 let setActiveElement = document.getElementById("addMovieForm"),
     btn = document.getElementById("addMovieButton");
 
@@ -5,3 +9,41 @@ let setActiveElement = document.getElementById("addMovieForm"),
 btn.addEventListener('click', () => {
     setActiveElement.classList.toggle("form--active");
 })
+
+
+
+// --------------- CRUD - Create --------------- //
+
+const localUrl = "http://localhost:3000/users";
+
+
+function loadUrl(url) 
+{
+  return new Promise(async function (resolve, reject) 
+  {
+    const getResolve = await fetch(url);
+
+    resolve(getResolve.json());
+
+  });
+}
+
+const promise = loadUrl(localUrl);
+
+promise.then((movies) => {
+    console.log(movies);
+
+    movies.forEach((movie) => {
+        
+        const html = 
+        `<div class="col-md-12 col-lg-6 col-xl-4 mt-5">
+            <div class="p-3 list__item list_item--border">
+                <h3>${movie.title}</h3>
+                <p>${movie.short_description}</p>
+                <p>${movie.long_description}</p>
+            </div>
+        </div>`
+
+        document.getElementById("movieList").insertAdjacentHTML("beforeend", html);
+    });
+});
