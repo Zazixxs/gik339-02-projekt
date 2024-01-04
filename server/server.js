403960977902f0c6c739a6c225e8e4c2252975f2
Zazixxs <125758932+Zazixxs@users.noreply.github.com>
2024-01-04 10:15:12 +0100

    config

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+/* Server objekt  */
+
+const server = express();
+
+/* Server inställningar */
+
+server
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    next();
+  });
+
+/* Starta Webbservern */
+
+server.listen(3000, () => {
+  console.log("Servern körs på port 3000");
+});
+sqlite3 = require("sqlite3").verbose();
